3322927690e169929b21e09743520f4bd8a983bf
Chris Kerbo <chris.kerbo@outlook.com>
2019-09-24 01:04:12 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -44,10 +44,15 @@ end
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
   holiday_hash[:winter].values.flatten
-
 end
 
 def all_supplies_in_holidays(holiday_hash)
+  holiday_hash.each do |holiday, item|
+    holiday.to_s
+    holiday.split(" ")
+    item.split(" ")
+    holiday_hash
+  end
   # iterate through holiday_hash and print items such that your readout resembles:
   # Winter:
   #   Christmas: Lights, Wreath
